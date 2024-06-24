a922fbb0d29372c815ad350b5fb64996528e158b
AlvinHartono <128015193+AlvinHartono@users.noreply.github.com>
2024-06-24 14:20:52 +0700

    feat: add get and post method in app.py

---
+++ backend/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+from flask_cors import CORS #Cross-Origin Resource Sharing
+import pickle
+
+
+app = Flask(__name__)
+CORS(app, resources={r"/*":{"origins":"*"}})
+
+model = pickle.load(open('ml_model.pkl', 'rb'))
+
+@app.route('/', methods=['GET'])
+def get_data():
+    data = {
+        "message":"API is Running"
+    }
+    return jsonify(data)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        data = request.get_json()
+        query_df = pd.DataFrame([data])
+        prediction = model.predict(query_df)
+        return jsonify({'Prediction': list(prediction)})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
+    
+    
+    
+# https://www.geeksforgeeks.org/integrating-a-ml-model-with-react-and-flask/
